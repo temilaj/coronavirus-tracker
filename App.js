5724eccdf187d3b644033e62bde7bdae8b268142
temilaj <temi@temilajumoke.com>
2020-05-01 18:31:41 +0100

    ch(setup): add fonts

--- App.js
+++ App.js
@@ -30,6 +30,11 @@ export default function App(props) {
         await Font.loadAsync({
           ...Ionicons.font,
           'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+          openSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+          'OpenSans-Light': require('./assets/fonts/OpenSans-Light.ttf'),
+          'OpenSans-SemiBold': require('./assets/fonts/OpenSans-SemiBold.ttf'),
+          'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+          'OpenSans-ExtraBold': require('./assets/fonts/OpenSans-ExtraBold.ttf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
